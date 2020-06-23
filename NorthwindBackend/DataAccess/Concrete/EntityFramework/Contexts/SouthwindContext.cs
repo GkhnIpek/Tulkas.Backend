34a84a8c0ce963dc927f03289567b164c5f8a640
GÖKHAN İPEK <gokipek91@gmail.com>
2020-06-23 15:47:12 +0300

    commit

---
+++ NorthwindBackend/DataAccess/Concrete/EntityFramework/Contexts/SouthwindContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Infrastructure.DataAccess.Abstract;
+using Infrastructure.Entities;
+using Infrastructure.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class SouthwindContext : DbContext, IDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=SouthwindContext;Trusted_Connection=true");
+        }
+        public DbSet<T> Set<T>() where T : class, IEntity
+        {
+            return base.Set<T>();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
